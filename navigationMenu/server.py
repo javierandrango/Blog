936fe7ed662dabbcec72657ea74aff2608896bec
javierandrango <lab_iarobotico593@outlook.com>
2024-01-11 12:09:46 -0500

    add new navigation menu bar

---
+++ navigationMenu/server.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "home page"
+
+@app.route('/electronics')
+def electronics():
+    return "electronics"
+
+@app.route('/artificial-intelligence')
+def ai():
+    return "artificial-intelligence"
+
+@app.route('/robotics')
+def robotics():
+    return "robotics"
+
+@app.route('/software')
+def software():
+    return "software"
+
+
+if  __name__ == "__main__":
+    app.debug = True
+    app.run(host='0.0.0.0', port=8080)
