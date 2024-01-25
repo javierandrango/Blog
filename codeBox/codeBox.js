f6a03efaad4ae2b3cb7cf11fa179cb28a45041d7
javierandrango <lab_iarobotico593@outlook.com>
2024-01-25 17:53:34 -0500

    add highlight style to the code

--- codeBox/codeBox.js
+++ codeBox/codeBox.js
@@ -1,4 +1,18 @@
+//highlights module
+//about the library: 
+//https://github.com/highlightjs/highlight.js#importing-the-library
 
+import hljs from 'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.9.0/build/es/highlight.min.js';
+/*
+import language_javascript from 'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.9.0/build/es/languages/javascript.min.js';
+import language_html from 'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.9.0/build/es/languages/xml.min.js';
+import language_c_plus_plus from 'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.9.0/build/es/languages/cpp.min.js';
+hljs.registerLanguage('javascript',language_javascript);
+hljs.registerLanguage('html',language_html);
+hljs.registerLanguage('C++',language_c_plus_plus);
+*/
+
+//numered code lines
 window.addEventListener("load",()=>{
     const codes = document.querySelectorAll('.custom-code');
     
@@ -18,9 +32,14 @@ window.addEventListener("load",()=>{
             let span = document.createElement('span');
             span.className = 'line';
             span.innerText = codeLines[j].replace(new RegExp(`^${whiteSpace}`), '');
+            
+            //highlight every code line
+            hljs.highlightElement(span);
+            span.style.backgroundColor = 'white';
+
+            //append new code line numered and styled
             codes[i].appendChild(span);
             codes[i].appendChild(document.createTextNode('\n'));
         }
     }
 })
-
