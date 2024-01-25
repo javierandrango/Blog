67f3951a6dd6140fdcd627ac223c35dea48e3fb6
javierandrango <lab_iarobotico593@outlook.com>
2024-01-24 23:26:10 -0500

    add numered code lines

--- codeBox/codeBox.js
+++ codeBox/codeBox.js
@@ -1,15 +1,27 @@
+
 window.addEventListener("load",()=>{
-    let codeContainer = document.getElementsByClassName('custom-codebox-container');
-    let lineNumbers = document.querySelector('.custom-line-numbers');
-    let codeLines = document.querySelector('.custom-code-body');
-    let numberCodeLines = codeLines.innerHTML.split('\n').length;
+    const codes = document.querySelectorAll('.custom-code-body');
     
-    console.log("number of code lines:",numberCodeLines);
-    for (let i=2; i<=numberCodeLines; i++){
-        let line = document.createElement('div');
-        line.innerText = i;
-        lineNumbers.appendChild(line);
+    for (let i=0; i<codes.length; i++){
+
+        //all code lines
+        const codeLines = codes[i].innerHTML.split('\n')
+        //extra white space in the first code line
+        const whiteSpace = codeLines[0].match(/^\s*/)[0]
+        
+        //delete the original code line
+        while(codes[i].childNodes.length > 0) {
+			codes[i].removeChild(codes[i].childNodes[0]);
+		}
+        //add numered code line without initial white space
+        for(let j=0; j < codeLines.length; j++){
+            let span = document.createElement('span');
+            span.className = 'line';
+            span.innerText = codeLines[j].replace(new RegExp(`^${whiteSpace}`), '');
+            codes[i].appendChild(span);
+            codes[i].appendChild(document.createTextNode('\n'));
+        }
     }
 
-    lineNumbers.style.height = codeContainer.scrollHeight + 'px';
 })
+
