4280c72709d83744c142b47dee26b8990383c7ab
javierandrango <lab_iarobotico593@outlook.com>
2024-01-26 21:12:22 -0500

    replace innerHTML with textContent to fix issue with special characters

--- codeBox/codeBox.js
+++ codeBox/codeBox.js
@@ -12,34 +12,35 @@ hljs.registerLanguage('html',language_html);
 hljs.registerLanguage('C++',language_c_plus_plus);
 */
 
-//numered code lines
-window.addEventListener("load",()=>{
-    const codes = document.querySelectorAll('.custom-code');
-    
-    for (let i=0; i<codes.length; i++){
+const codes = document.querySelectorAll('.custom-code');
+if (codes){
+    //numered code lines
+    window.addEventListener("load",()=>{
+        for (let i=0; i<codes.length; i++){
 
-        //all code lines
-        const codeLines = codes[i].innerHTML.split('\n')
-        //extra white space in the first code line
-        const whiteSpace = codeLines[0].match(/^\s*/)[0]
-        
-        //delete the original code line
-        while(codes[i].childNodes.length > 0) {
-			codes[i].removeChild(codes[i].childNodes[0]);
-		}
-        //add numered code line without initial white space
-        for(let j=0; j < codeLines.length; j++){
-            let span = document.createElement('span');
-            span.className = 'line';
-            span.innerText = codeLines[j].replace(new RegExp(`^${whiteSpace}`), '');
-            
-            //highlight every code line
-            hljs.highlightElement(span);
-            span.style.backgroundColor = 'white';
+            //all code lines
+            const codeLines = codes[i].textContent.split('\n')
+            //extra white space in the first code line
+            const whiteSpace = codeLines[0].match(/^\s*/)[0]
 
-            //append new code line numered and styled
-            codes[i].appendChild(span);
-            codes[i].appendChild(document.createTextNode('\n'));
+            //delete the original code line
+            while(codes[i].childNodes.length > 0) {
+    			codes[i].removeChild(codes[i].childNodes[0]);
+    		}
+            //add numered code line without initial white space
+            for(let j=0; j < codeLines.length; j++){
+                let span = document.createElement('span');
+                span.className = 'line';
+                span.innerText = codeLines[j].replace(new RegExp(`^${whiteSpace}`), '');
+
+                //highlight every code line
+                hljs.highlightElement(span);
+                span.style.backgroundColor = 'white';
+
+                //append new code line numered and styled
+                codes[i].appendChild(span);
+                codes[i].appendChild(document.createTextNode('\n'));
+            }
         }
-    }
-})
+    })
+}
